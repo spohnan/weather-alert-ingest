d1f4de7706bd8236950dbdd75c82bfd7feca6875
Andy Spohn <Andy@AndySpohn.com>
2014-11-20 19:02:38 -0500

    Simple working example

--- src/main/scala/com/andyspohn/wai/HdfsWordCount.scala
+++ src/main/scala/com/andyspohn/wai/HdfsWordCount.scala
@@ -15,24 +15,12 @@
  * limitations under the License.
  */
 
-package org.apache.spark.examples.streaming
+package com.andyspohn.wai
 
-import com.andyspohn.wai.StreamingExamples
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.StreamingContext._
 
-/**
- * Counts words in new text files created in the given directory
- * Usage: HdfsWordCount <directory>
- *   <directory> is the directory that Spark Streaming will use to find and read new text files.
- *
- * To run this on your local machine on directory `localdir`, run this example
- *    $ bin/run-example \
- *       org.apache.spark.examples.streaming.HdfsWordCount localdir
- *
- * Then create a text file in `localdir` and the words in the file will get counted.
- */
 object HdfsWordCount {
   def main(args: Array[String]) {
     if (args.length < 1) {
@@ -42,11 +30,7 @@ object HdfsWordCount {
 
     StreamingExamples.setStreamingLogLevels()
     val sparkConf = new SparkConf().setAppName("HdfsWordCount")
-    // Create the context
     val ssc = new StreamingContext(sparkConf, Seconds(2))
-
-    // Create the FileInputDStream on the directory and use the
-    // stream to count words in new files created
     val lines = ssc.textFileStream(args(0))
     val words = lines.flatMap(_.split(" "))
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
